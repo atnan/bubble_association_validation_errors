4c587b943a4fc26726b3880a5b11125e8d2ba3df
Nathan de Vries <nathan@atnan.com>
2008-09-29 21:35:30 +1000

    Initial commit.

---
+++ init.rb
@@ -0,0 +1,22 @@
+# http://pivots.pivotallabs.com/users/nick/blog/articles/359-alias-method-chain-validates-associated-informative-error-message
+# http://pastie.caboo.se/142774
+
+module ActiveRecord::Validations::ClassMethods
+  def validates_associated(*associations)
+    associations.each do |association|
+      class_eval do
+        validates_each(associations) do |record, associate_name, value|
+          associates = record.send(associate_name)
+          associates = [associates] unless associates.respond_to?('each')
+          associates.each do |associate|
+            if associate && !associate.valid?
+              associate.errors.each do |key, value|
+                record.errors.add(key, value)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
